99bada9c22729124c74b43d8105b319267f669c0
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2023-06-22 14:10:52 -0300

    Finalizando a estrutura da janela do sistema

--- sistemaCotacao.py
+++ sistemaCotacao.py
@@ -7,6 +7,14 @@ def pegar_cotacao():
     pass
 
 
+def selecionar_arquivo():
+    pass
+
+
+def atualizar_cotacoes():
+    pass
+
+
 lista_moedas = ['USD', 'EUR']
 
 janela = tk.Tk()
@@ -17,13 +25,13 @@ janela.title('Ferramenta de Cotação de Moedas')
 label_cotacaomoeda = tk.Label(text='Cotação de 1 Moeda Específica', borderwidth=2, relief='solid')
 label_cotacaomoeda.grid(row=0, column=0, padx=10, pady=10, sticky='nswe', columnspan=3)
 
-label_selecionarmoeda = tk.Label(text='Selecionar Moeda')
+label_selecionarmoeda = tk.Label(text='Selecionar Moeda', anchor='e')
 label_selecionarmoeda.grid(row=1, column=0, padx=10, pady=10, sticky='nswe', columnspan=2)
 
 combobox_selecionarmoeda = ttk.Combobox(values=lista_moedas)
 combobox_selecionarmoeda.grid(row=1, column=2, padx=10, pady=10, sticky='nswe')
 
-label_selecionardia = tk.Label(text='Selecione o dia que deseja pegar a cotação')
+label_selecionardia = tk.Label(text='Selecione o dia que deseja pegar a cotação', anchor='e')
 label_selecionardia.grid(row=2, column=0, padx=10, pady=10, sticky='nswe', columnspan=2)
 
 calendario_moeda = DateEntry(year=2023, locale='pt_br')
@@ -38,5 +46,33 @@ botao_pegarcotacao.grid(row=3, column=2, padx=10, pady=10, sticky='nswe')
 # Cotação de várias moedas
 label_cotacaovariasmoedas = tk.Label(text='Cotação de 1 Múltiplas Moedas', borderwidth=2, relief='solid')
 label_cotacaovariasmoedas.grid(row=4, column=0, padx=10, pady=10, sticky='nswe', columnspan=3)
+
+label_selecionararquivo = tk.Label(text='Selecione um arquivo em Excel com as moedas na coluna A')
+label_selecionararquivo.grid(row=5, column=0, padx=10, pady=10, columnspan=2, sticky='nswe')
+
+botato_selecionararquivo = tk.Button(text='Selecionar Arquivo', command=selecionar_arquivo)
+botato_selecionararquivo.grid(row=5, column=2, padx=10, pady=10, sticky='nswe')
+
+label_arquivoselecionado = tk.Label(text='Nenhum Arquivo Selecionado', anchor='e')
+label_arquivoselecionado.grid(row=6, column=0, columnspan=3, padx=10, pady=10, sticky='nswe')
+
+label_datainicial = tk.Label(text='Data Inicial', anchor='e')
+label_datafinal = tk.Label(text='Data Final', anchor='e')
+label_datainicial.grid(row=7, column=0, padx=10, pady=10, sticky='nswe')
+label_datafinal.grid(row=8, column=0, padx=10, pady=10, sticky='nswe')
+
+calendario_datainicial = DateEntry(year=2023, locale='pt_br')
+calendario_datafinal = DateEntry(year=2023, locale='pt_br')
+calendario_datainicial.grid(row=7, column=1, padx=10, pady=10, sticky='nswe')
+calendario_datafinal.grid(row=8, column=1, padx=10, pady=10, sticky='nswe')
+
+botao_atualizarcotacoes = tk.Button(text='Atualizar Cotações', command=atualizar_cotacoes)
+botao_atualizarcotacoes.grid(row=9, column=0, padx=10, pady=10, sticky='nswe')
+
+label_atualizarcotacoes = tk.Label(text='')
+label_atualizarcotacoes.grid(row=9, column=1, columnspan=2, padx=10, pady=10, sticky='nswe')
+
+botao_fechar = tk.Button(text='Fechar', command=janela.quit)
+botao_fechar.grid(row=10, column=2, padx=10, pady=10, sticky='nswe')
 
 janela.mainloop()
